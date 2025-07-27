d7739db9ad82bbf3cadebd195ef3eb88449ab3ef
Shenghong <me@zhoud2001.io>
2025-07-27 00:23:37 -0400

    Initial Commit

---
+++ chat-bot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chat-bot
+//
+//  Created by Shenghong Zhou on 7/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chat-bot/chat_botApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_botApp.swift
+//  chat-bot
+//
+//  Created by Shenghong Zhou on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct chat_botApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
